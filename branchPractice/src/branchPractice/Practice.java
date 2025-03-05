acc48e7f8950bd5422267019642e289f2295494c
amani.fuqua <amani.fuqua@MU-C1-07.mullen.pvt.k12.co.us>
2025-03-05 13:20:54 -0700

    first push practice

---
+++ branchPractice/src/branchPractice/Practice.java
@@ -0,0 +1,12 @@
+package branchPractice;
+
+public class Practice
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+				System.out.println("master branch");
+			}
+
+	}
